c3f1ef7018687c4f7d51805650a44c15c2f4bff4
Hayes Crowley <in5607@Loxatl0v089jg5h.local>
2019-10-01 17:05:11 -0400

    app is working, need to finish out features and finalize styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,62 +1,63 @@
-// When you click the create Note button 
+// When you click the create Note button
 $(document).on("click", "#createNote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-    $(`#note[data-id=${thisId}]`).show();
-    // Change the id to save note
-    $(this).attr("id", "saveNote");
-    $(this).text("Save Note");
+  alert("create note clicked");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+  $(`#noteTaker[data-id=${thisId}]`).show();
+  // Change the id to save note
+  $(this).attr("id", "saveNote");
+  $(this).text("Save Note");
 });
 
 // When you click the savenote button
 $(document).on("click", "#saveNote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-    var bodyInput = $("#bodyInput").val();
-    var titleInput = $("#titleInput").val();
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-            method: "POST",
-            url: "/api/articles/" + thisId,
-            data: {
-                // Value taken from title input
-                title: $("#titleInput").val(),
-                // Value taken from note textarea
-                body: $("#bodyInput").val()
-            }
-        })
-        // With that done
-        .then(function(data) {
-            // Log the response
-            console.log(data);
-            // hide note taker, revert button id, add text into note area 
-            $(`#noteTaker[data-id=${thisId}]`).hide();
-            $(this).attr("id", "createNote");
-            $(`#noteAreaTitle[data-id=${thisId}]`).text(titleInput);
-            $(`#noteAreaBody[data-id=${thisId}]`).text(bodyInput);
-        });
+  alert("saved note clicked");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+  var bodyInput = $("#bodyInput").val();
+  var titleInput = $("#titleInput").val();
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/api/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: titleInput,
+      // Value taken from note textarea
+      body: bodyInput
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      // hide note taker, revert button id, add text into note area
+      $(`#noteTaker[data-id=${thisId}]`).hide();
+      $(this).attr("id", "createNote");
+      $(`#noteAreaTitle[data-id=${thisId}]`).text(titleInput);
+      $(`#noteAreaBody[data-id=${thisId}]`).text(bodyInput);
+    });
 });
 
-// When you click the Save Article button 
+// When you click the Save Article button
 $(document).on("click", "#saveArticle", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
 
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-            method: "POST",
-            url: "/api/articles/save/" + thisId,
-            data: {
-                // Value taken from title input
-                saved: true
-            }
-        })
-        // With that done
-        .then(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            $(this).css("pointer-events", "none");
-            $(this).text("Saved!");
-        });
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/api/articles/save/" + thisId,
+    data: {
+      // Value taken from title input
+      saved: true
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $(this).css("pointer-events", "none");
+      $(this).text("Saved!");
+    });
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,41 +1,42 @@
-var express = require('express'),
-    router = express.Router();
+var express = require("express"),
+  router = express.Router();
 
-var db = require('../models');
+var db = require("../models");
 
-router
-    .get('/', function(_req, res) {
-        res.render("index", {
-            styles: ["normalize.css", "nav.css", "styles.css"],
-            headScripts: ["jquery.min.js", "nav.js"],
-            bodyScripts: [],
-            title: "About | Mongo Scraper"
-        });
-    });
+router.get("/", function(_req, res) {
+  res.render("index", {
+    styles: ["normalize.css", "nav.css", "styles.css"],
+    headScripts: ["jquery.min.js", "nav.js"],
+    bodyScripts: [],
+    title: "About | Mongo Scraper"
+  });
+});
 
-// for viewing articles json 
+// for viewing articles json
 router.get("/articles", function(req, res) {
-    db.Article.find().then((articles) => {
-        res.render("scrapedArticles", {
-            styles: ["normalize.css", "nav.css", "styles.css"],
-            headScripts: ["jquery.min.js", "nav.js"],
-            bodyScripts: ["app.js"],
-            title: "Results | Mongo Scraper",
-            articles: articles
-        });
+  db.Article.find()
+    .populate("note")
+    .then(articles => {
+      res.render("scrapedArticles", {
+        styles: ["normalize.css", "nav.css", "styles.css"],
+        headScripts: ["jquery.min.js", "nav.js"],
+        bodyScripts: ["app.js"],
+        title: "Results | Mongo Scraper",
+        articles: articles
+      });
     });
 });
 
-router.get("/saved-articles", function(req, res) {
-    db.Article.find({ "saved": true }).then((articles) => {
-        res.render("savedArticles", {
-            styles: ["normalize.css", "nav.css", "styles.css"],
-            headScripts: ["jquery.min.js", "nav.js"],
-            bodyScripts: [],
-            title: "Results | Mongo Scraper",
-            articles: articles
-        });
+router.get("/savedArticles", function(req, res) {
+  db.Article.find({ saved: true }).then(articles => {
+    res.render("savedArticles", {
+      styles: ["normalize.css", "nav.css", "styles.css"],
+      headScripts: ["jquery.min.js", "nav.js"],
+      bodyScripts: [],
+      title: "Results | Mongo Scraper",
+      articles: articles
     });
+  });
 });
 
 module.exports = router;
